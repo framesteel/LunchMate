b1df96e3b72ed6547b893cf96a049bb430a899fc
colin <cjframe1@gmail.com>
2018-10-20 18:02:28 -0400

    Getting location data

--- screens/Nearme.js
+++ screens/Nearme.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
 
+
+const API_Key = "AIzaSyAelcE44NB-3d40mpX2UOq87ueXFhD8DgM";
 export default class Nearme extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      position: {
+        latitude: 0,
+        longitude: 0
+      },
+      places;
+    }
+  }
+
+  async requestLocationPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        'title': 'LunchMate requests location information',
+        'message': 'LunchMate needs your location to give you sick deals near you!'
+      }
+    )
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the location")
+    } else {
+      console.log("Location permission denied")
+    }
+    }
+    catch (err) {
+      console.warn(err)
+  }
+  }
+
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+      this.setState({
+        position: {
+          latitude: lat,
+          longitude: long
+        }
+      })
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Nearme!</Text>
+        <Text>Lat: {this.state.position.latitude}</Text>
+        <Text>Long: {this.state.position.longitude}</Text>
       </View>
     );
   }
